b446681245f8e8a542656840befd9a282e1528fb
Cesar Varela <cesarac66@hotmail.com>
2020-03-20 16:58:49 -0600

    Print ENV variable to look what we have

--- src/main/java/mx/com/beethub/receipt/ReceiptApiApplication.java
+++ src/main/java/mx/com/beethub/receipt/ReceiptApiApplication.java
@@ -1,16 +1,24 @@
 package mx.com.beethub.receipt;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import java.util.Map;
+
 
 @SpringBootApplication
 @EnableFeignClients
-public class ReceiptApiApplication {
+public class ReceiptApiApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReceiptApiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Map<String, String> env = System.getenv();
+		env.forEach((k, v) -> System.out.println(k + ":" + v));
+	}
 }
