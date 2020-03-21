1067188e5fc8c5b114280a08c0799cde1cbf51c8
Cesar Varela <cesarac66@hotmail.com>
2020-03-20 18:35:18 -0600

    Re test the deploy

--- src/main/java/mx/com/beethub/receipt/ReceiptApiApplication.java
+++ src/main/java/mx/com/beethub/receipt/ReceiptApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import java.util.Map;
 
+
 @SpringBootApplication
 @EnableFeignClients
 public class ReceiptApiApplication implements CommandLineRunner {
