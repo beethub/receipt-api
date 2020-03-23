cc0ff417cef9c175b9f6c2e155fde7a5c51a7390
Cesar Varela <cesarac66@hotmail.com>
2020-03-23 12:27:15 -0600

    Add configuration for the data base to conect

--- src/main/java/mx/com/beethub/receipt/ReceiptApiApplication.java
+++ src/main/java/mx/com/beethub/receipt/ReceiptApiApplication.java
@@ -1,24 +1,15 @@
 package mx.com.beethub.receipt;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-import java.util.Map;
-
-
 @SpringBootApplication
 @EnableFeignClients
-public class ReceiptApiApplication implements CommandLineRunner {
+public class ReceiptApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReceiptApiApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) {
-		Map<String, String> env = System.getenv();
-		env.forEach((k, v) -> System.out.println(k + ":" + v));
-	}
 }
